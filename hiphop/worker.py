245241ae5c24f4827741075ba5389042fa7920c0
John T <john.david.tringham@gmail.com>
2023-10-23 23:22:10 +0100

    got API working, and polars set-up. Now can pass information on a playlist into a polars df.

--- hiphop/worker.py
+++ hiphop/worker.py
@@ -1,3 +1,5 @@
+import polars as pl
+
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 from common.constants.client_info import ClientInfoConstants as cic
@@ -6,6 +8,7 @@ from common.constants.client_info import ClientInfoConstants as cic
 
 # TODO - edit the python venv to 3.11
 # Credits to https://towardsdatascience.com/extracting-song-data-from-the-spotify-api-using-python-b1e79388d50 for a solid starting point
+
 
 def pipeline(playlist_link):
     """
@@ -17,7 +20,9 @@ def pipeline(playlist_link):
     client_credentials_manager = SpotifyClientCredentials(client_id=cic.cid, client_secret=cic.secret)
     sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
-    extract_songs(playlist_link=playlist_link, sp=sp)
+    track_dict = extract_songs(playlist_link=playlist_link, sp=sp)
+
+    create_polars_dataframe(track_dict=track_dict)
 
 
 def extract_songs(playlist_link, sp):
@@ -27,29 +32,49 @@ def extract_songs(playlist_link, sp):
     :param sp:
     :return:
     """
+    track_dict = {
+        'track_uri': [],
+        'track_name': [],
+        'artist_uri': [],
+        'artist_info': [],
+        'artist_name': [],
+        'artist_pop': [],
+        'artist_genres': [],
+        'album': [],
+        'track_pop': []
+    }
+
     playlist_URI = playlist_link.split("/")[-1].split("?")[0]
     track_object = sp.playlist_tracks(playlist_URI)["items"]
+
     for track in track_object:
         # URI
-        track_uri = track["track"]["uri"]
+        track_dict['track_uri'] += [track["track"]["uri"]]
 
         # Track name
-        track_name = track["track"]["name"]
+        track_dict['track_name'] += [track["track"]["name"]]
 
         # Main Artist
         artist_uri = track["track"]["artists"][0]["uri"]
-        artist_info = sp.artist(artist_uri)
+        track_dict['artist_uri'] += [artist_uri]
+        track_dict['artist_info'] += [sp.artist(artist_uri)]
 
         # Name, popularity, genre
-        artist_name = track["track"]["artists"][0]["name"]
-        artist_pop = artist_info["popularity"]
-        artist_genres = artist_info["genres"]
+        track_dict['artist_name'] += [track["track"]["artists"][0]["name"]]
+        track_dict['artist_pop'] += [(sp.artist(artist_uri)["popularity"])]
+        track_dict['artist_genres'] += [sp.artist(artist_uri)["genres"]]
 
         # Album
-        album = track["track"]["album"]["name"]
+        track_dict['album'] += [track["track"]["album"]["name"]]
 
         # Popularity of the track
-        track_pop = track["track"]["popularity"]
+        track_dict['track_pop'] += [(track["track"]["popularity"])]
+
+    return track_dict
+
+def create_polars_dataframe(track_dict):
+    df = pl.DataFrame(track_dict)
+    return df
 
 if __name__ == '__main__':
     playlist_link = "https://open.spotify.com/playlist/37i9dQZEVXbNG2KDcFcKOF?si=1333723a6eff4b7f"
